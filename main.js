e8f8b13cae4246957a903dd16fa19c3332d9d6ef
Akshay-Sarvang19 <104013269+Akshay-Sarvang19@users.noreply.github.com>
2022-05-01 10:01:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function bedroom() {
+    window.location = "bedroom.html";
+}
+function bottle() {
+    window.location = "bottle.html";
+}
+function laptop() {
+    window.location = "laptop.html";
+}
+function bags() {
+    window.location = "bags.html";
+}
